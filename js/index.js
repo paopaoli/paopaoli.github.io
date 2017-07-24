ba493a322595a6ab5d5d7e18b7b8a6d5301d2993
emidiotorre <emidio.torre@gmail.com>
2017-07-24 22:46:28 +0200

    added cookie handler

--- js/index.js
+++ js/index.js
@@ -67,8 +67,36 @@ if(IE) {
 }
 
 jQuery(document).ready(function(){
-  jQuery('#overlay').click(function(e){
-    jQuery('#overlay').fadeOut();
-    jQuery('#overlay').delay(400).remove();
-  })
+  if(getCookie('opened')==1){
+      jQuery('#overlay').remove();
+  }else{
+    jQuery('#overlay').click(function(e){
+      setCookie('opened',1,1);
+      jQuery('#overlay').fadeOut();
+      jQuery('#overlay').delay(400).remove();
+    })
+  }
 });
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
