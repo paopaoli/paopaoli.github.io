ae6c8a18287ad059cb6c1cd27e7d2233f6c528ca
emidiotorre <emidio.torre@gmail.com>
2017-07-24 22:56:57 +0200

    added cookie handler

--- js/index.js
+++ js/index.js
@@ -79,9 +79,9 @@ jQuery(document).ready(function(){
   }
 });
 
-function setCookie(cname, cvalue, exdays) {
+function setCookie(cname, cvalue, exhours) {
     var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    d.setTime(d.getTime() + (exhours*60*60*1000));
     var expires = "expires="+ d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
